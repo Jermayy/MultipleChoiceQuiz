adc923e3f68314687f4b66f5b499faa28a6fa84b
Jeremy <jeremyVDLRC@gmail.com>
2020-06-13 23:33:52 +1000

    added 4 questions to use in quiz and pseudo code for functionality of quiz

---
+++ javascript/indexScript.js
@@ -0,0 +1,28 @@
+//  Questions:
+
+// 1.Commonly used data types DO NOT include: [1.strings, 2.booleans, 3. alerts, 4.numbers]
+// 2.The condition in an if/else statement is enclosed within ____. [1.quotes, 2.curly brackets, 3.parentheses, 4.square brackets]
+// 3. Arrays in Javascript can be used to store _____. [1. numbers and strings, 2. other arrays, 3.booleans, 4.all of the above]
+// 4. String values must be enclosed within ____ when being assigned to variables [1.commas, 2. curly brackets, 3.quotes 4.parentheses]
+// 5. A very useful tool used during development and debugging for printing content to the debugger is: [1.Javascript, 2.terminal/bash 3.for loops 4.console.log]
+
+
+
+//  1.When user opens quiz app
+//     - Load start button and text '1 minute to answer all questions'
+// 2.When user clicks start button
+//     - load first question in array 
+//     - loan question options 
+//     - start timer (60 seconds)
+// 3. when user clicks an answer option
+//     - check if answer is correct and display 'correct' or 'wrong'
+//     - if answer is WRONG: add to counter for point deduction at end of quiz
+//     - load next question
+
+//  4. When user answers final question:
+//     - show if answer is correct or Not
+//     - deduct any points from remaining time left to calculate score
+//     - display score to user and display input for user to add name
+
+//  5. when user enters name
+//     - Load scoreboard.html with previous highscores and names in descending order of scores
